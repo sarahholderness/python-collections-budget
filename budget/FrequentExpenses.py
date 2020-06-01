c25e9bc012128c6d4f1e440a5e67c78946611e77
sarah <sarah-holderness@pluralsight.com>
2020-06-01 19:39:05 -0400

    Module 1 Task 5-6

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -8,3 +8,5 @@ spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
 spending_counter = collections.Counter(spending_categories)
+top5 = spending_counter.most_common(5)
+categories, count = zip(*top5)
