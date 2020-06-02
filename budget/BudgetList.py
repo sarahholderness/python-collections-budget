03881eb746e3cd4b7626792e4b617d29eab07a84
sarah <sarah-holderness@pluralsight.com>
2020-06-01 20:02:21 -0400

    Module 2 Tasks 6-12

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,3 +1,5 @@
+from . import Expense
+
 class BudgetList:
     def __init__(self, budget):
         self.budget = budget
@@ -13,3 +15,18 @@ class BudgetList:
         else:
             self.overages.append(item)
             self.sum_overages += item
+
+    def __len__(self):
+        return len(self.expenses) + len(self.overages)
+
+def main():
+    myBudgetList = BudgetList(1200)
+    expenses = Expense.Expenses()
+    expenses.read_expenses('data/spending_data.csv')
+    for expense in expenses.list:
+        myBudgetList.append(expense.amount)
+
+    print('The count of all expenses: ' + str(len(myBudgetList)))
+
+if __name__ == '__main__':
+    main()
