1dd670c4de289d2fd79d1f852e452ca89e2cdfdb
sarah <sarah-holderness@pluralsight.com>
2020-06-01 20:19:29 -0400

    Module 4 Tasks 1-7

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -5,7 +5,9 @@ def main():
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
+    divided_set_comp = expenses.categorize_set_comprehension()
 
+    
 
 if __name__ == "__main__":
     main()
